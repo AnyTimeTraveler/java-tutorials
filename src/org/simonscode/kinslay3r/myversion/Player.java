0d7717ef02577934b50cdcc6f593389ebd21c66d
Simon Struck <simon@simonscode.org>
2019-09-09 11:12:10 +0200

    Add comments to my Player class

--- src/org/simonscode/kinslay3r/myversion/Player.java
+++ src/org/simonscode/kinslay3r/myversion/Player.java
@@ -1,13 +1,25 @@
 package org.simonscode.kinslay3r.myversion;
 
 public class Player {
-    private String name;
-    private double height;
+    // I made the fields final, since I am not expecting either of them to change and
+    // if they should have to change, the compiler will not allow me to and
+    // therefore make me recheck the usages of the field
+    private final String name;
 
-    public Player(String name, double height) {
+    // I just simplified the height to a double,
+    // representing the height in meters
+    private final double height;
+
+    // Take the values for the fields in the constructor
+    // I added the 'final's here, since the arguments are read-only and should allow compiler optimisation
+    // This is not required, though
+    public Player(final String name, final double height) {
         this.name = name;
         this.height = height;
     }
+
+    // Since the values of name and height are unlikely to change,
+    // I only allow them to be set in the constructor and leave out setters
 
     public String getName() {
         return name;
